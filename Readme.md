4f87202fedb477f264a73dac6cde01cd97910f33
Steven Moazami <mong8se@users.noreply.github.com>
2022-09-02 16:15:40 -0400

    initial version

---
+++ Readme.md
@@ -0,0 +1,57 @@
+# Actually.fish
+
+![A cat saying "umm... actually" and pushing up its nerd glasses](https://media.giphy.com/media/RMwZypp489fuGBI0Ti/giphy.gif)
+
+## What is it?
+
+Idea shamelessly stolen from:
+[EinfachToll/DidYouMean](https://github.com/EinfachToll/DidYouMean) and
+applied to changing directories in fish shell.
+
+## What it do?
+
+Let's say you want to change directory to `somedir`, so you type:
+
+```fish
+
+$ cd some<TAB><CR>
+
+```
+
+But! You forgot about the existence of `some_other_dir`! (heck maybe
+there is even a `some_forgotten_dir`)
+After the `<TAB>` the shell prompted you to disambiguate ... but you
+already swiftly hit `<CR>` so you get an error:
+`cd: The directory “some” does not exist`
+... womp womp
+
+But wait! With this plugin you'll get a prompt asking you if you
+actually want to choose `somedir` or `some_other_dir` (or however many
+matches)
+
+But don't worry, you can cancel if none of it is what you wanted.
+
+## How I install?
+
+The regular way.
+
+```fish
+fisher install "mong8se/actually.fish"
+```
+
+## How I configure
+
+Ain't none.
+
+## Dependencies?
+
+Hard dependency on `find` . 
+
+Soft dependency on `fzf` (if you don't have it, it'll fall back to a
+fish `read` prompt)
+
+Even softer on `lsd` (if you have fzf, it'll preview the directories
+with `lsd` if you have it but fallback to `ls` if not)
+
+TODO: Cleanup
+TODO: Windows Operating System!?
---
+++ conf.d/actually.fish
@@ -0,0 +1,61 @@
+status is-interactive || exit
+
+function _actually --on-event fish_postexec
+  set -f last_status $status
+  if test $last_status -eq 0
+    return 0
+  end
+
+  set -f last_cmd (string split " " $argv[1])
+  if test "cd" != "$last_cmd[1]"
+    return $last_status
+  end
+
+  # using string replace to do tilde expansion manually, not sure if there is a way
+  # to get it to work automatically when inside a variable from a split string
+  set -f last_dir (path dirname $last_cmd[2] | string replace -r '^~' "$HOME")
+  set -f last_base (path basename $last_cmd[2])
+  set -l list (command find "$last_dir" -maxdepth 1 -mindepth 1 -name "$last_base"'*' | path sort)
+
+  if test -z "$list"
+    return $last_status
+  end
+
+  set_color blue
+  echo um ... actually, you probably meant:
+  set_color normal
+
+  set -f destination
+  if type -q fzf
+    set -l preview
+    if type -q lsd
+      set preview 'lsd --color always --icon always {}'
+    else
+      set preview 'ls -F --color {}'
+    end
+    set destination (string join0 $list | fzf --read0 --no-info --no-sort --preview="$preview")
+  else
+    set -l i 1
+    for dir in $list
+      echo $i $dir
+      set i (math $i + 1)
+    end
+    set -l answer (read -n 1 -p 'set_color green; echo -n "> " ; set_color normal')
+
+    if string match -r '^\d+$' "$answer"
+      and test $answer -gt 0
+      set destination "$list[$answer]"
+    end
+  end
+
+  if test -d "$destination"
+    cd $destination
+  else
+    echo nevermind
+    return $last_status
+  end
+end
+
+function _actually_uninstall --on-event actually_uninstall
+  functions --erase _actually _actually_uninstall
+end
