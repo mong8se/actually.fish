05db54251e71c1101057b4a30ae3c2e0057b74d4
Steven Moazami <mong8se@users.noreply.github.com>
2022-09-03 10:36:13 -0400

    adding back in the whole reason to use find in the first place

--- conf.d/actually.fish
+++ conf.d/actually.fish
@@ -15,7 +15,7 @@ function _actually --on-event fish_postexec
   # to get it to work automatically when inside a variable from a split string
   set -f last_dir (path dirname $last_cmd[2] | string replace -r '^~' "$HOME")
   set -f last_base (path basename $last_cmd[2])
-  set -l list (command find "$last_dir" -maxdepth 1 -mindepth 1 -iname "$last_base"'*' | path sort)
+  set -l list (command find "$last_dir" -maxdepth 1 -mindepth 1 -type d -iname "$last_base"'*' | path sort)
 
   if test -z "$list"
     return $last_status
